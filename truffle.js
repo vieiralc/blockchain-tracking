385338bc5fb84a239a74829be28927d8ff3febcf
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-12-10 09:57:04 -0200

    update truffle config

--- truffle.js
+++ truffle.js
@@ -1,11 +1,33 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  
   networks: {
     development: {
       host: "localhost",
       port: "7545",
       network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.infuraKey}`
+        );
+      },
+      gasPrice: 25000000000,
+      network_id: 3
+    }
+
+  },
+
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
