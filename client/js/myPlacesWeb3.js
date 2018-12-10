026fc94a4934a5c96f0e9c4525931f5a2746fe72
= <=>
2018-12-10 14:50:01 -0200

    update smart contract address

--- client/js/myPlacesWeb3.js
+++ client/js/myPlacesWeb3.js
@@ -20,7 +20,7 @@ window.addEventListener('load', function() {
 });
 
 async function startApp() {
-    const contractAddress = "0x2c2b9c9a4a25e24b174f26114e8926a9f2128fe4";
+    const contractAddress = "0xB386d866Df58ADe84E0A2aF87e7170d237b900e1";
 
     // contractABI is at contracts/contract_abi.js
     contract = new web3.eth.Contract(contractABI, contractAddress);
