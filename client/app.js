26c8edcd5935e3a9943896f0772ca5c9d5df4204
= <=>
2018-12-11 14:02:02 -0200

    update render

--- client/app.js
+++ client/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
